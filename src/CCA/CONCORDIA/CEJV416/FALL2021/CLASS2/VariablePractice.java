5ff419c679151f5fe099bc139226d21baf9e388d
Aaron <cherongwa@gmail.com>
2021-09-16 19:51:51 -0400

    Initial commit

---
+++ src/CCA/CONCORDIA/CEJV416/FALL2021/CLASS2/VariablePractice.java
@@ -0,0 +1,19 @@
+package CCA.CONCORDIA.CEJV416.FALL2021.CLASS2;
+
+public class VariablePractice {
+   /* this is some
+            aijeija*/
+
+    public static void main(String[] args) {
+        double GASuSAGE =1.1;
+        float waterUsage = 1.2f;
+
+        int counter;
+        counter =0;
+
+        int counter2 =0;
+
+
+    }
+}
+
---
+++ src/CCA/CONCORDIA/CEJV416/FALL2021/Main.java
@@ -0,0 +1,8 @@
+package CCA.CONCORDIA.CEJV416.FALL2021;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
