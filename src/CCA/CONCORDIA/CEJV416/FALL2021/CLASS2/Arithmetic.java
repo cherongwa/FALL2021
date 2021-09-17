6414d27faa367d79d5a6a76a623cbd46e2029f1b
Aaron <cherongwa@gmail.com>
2021-09-16 21:03:57 -0400

    END OF CLASS 2

---
+++ src/CCA/CONCORDIA/CEJV416/FALL2021/CLASS2/Arithmetic.java
@@ -0,0 +1,20 @@
+package CCA.CONCORDIA.CEJV416.FALL2021.CLASS2;
+
+public class Arithmetic {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 2;
+
+        int c = a + b;
+
+        double d = a / b;
+
+        System.out.println(c);
+
+        double e = 5.0 / 2;
+
+        double f = (double) 5/2;
+        // casting = five is casted to a double
+    }
+}
--- src/CCA/CONCORDIA/CEJV416/FALL2021/CLASS2/VariablePractice.java
+++ src/CCA/CONCORDIA/CEJV416/FALL2021/CLASS2/VariablePractice.java
@@ -1,5 +1,7 @@
 package CCA.CONCORDIA.CEJV416.FALL2021.CLASS2;
 
+import java.math.BigDecimal;
+
 public class VariablePractice {
    /* this is some
             aijeija*/
@@ -13,6 +15,47 @@ public class VariablePractice {
 
         int counter2 =0;
 
+        boolean expired = true;
+
+        char my_initial = 'B';
+        // char is a type
+        //string is a reference ( a class)
+
+        int a = 1;
+        int b = 2;
+        int c = a+b;
+
+        double d = a/b;
+        double e = 5.0/2;
+        double f = (double)5 / 2;
+
+
+        //sale is a class
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        int x = 4;
+        long y = 22;
+
+        int z = x + (int)y;
+
+        System.out.println(z);
+
+        double p = Math.pow(2.0,2.0);
+
+        System.out.println(p);
+
+        double radius = 5.0;
+        double circleArea = Math.PI* (radius * radius);
+
+        double circleArea2 = Math.pow(Math.PI * radius, 2.0);
+
+        BigDecimal costOfMeal = new BigDecimal (  "22.99");
+
+        System.out.println(costOfMeal);
+        costOfMeal = costOfMeal.add(new BigDecimal("1.0"));
 
     }
 }
