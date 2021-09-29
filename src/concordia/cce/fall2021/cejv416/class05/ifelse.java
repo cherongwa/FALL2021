31fc8b2a52c897eb4958de7b79c7081707bf4a07
Aaron <cherongwa@gmail.com>
2021-09-28 21:23:16 -0400

    28 september

---
+++ src/concordia/cce/fall2021/cejv416/class05/ifelse.java
@@ -0,0 +1,14 @@
+package concordia.cce.fall2021.cejv416.class05;
+
+public class ifelse {
+    public static void main(String[] args) {
+
+        int numPPL =20;
+        if (numPPL <50)
+        {
+            System.out.println("A");
+        }
+
+
+    }
+}
